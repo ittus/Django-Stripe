fb953ce0d81ce44768666d0795e47aa94c793ec7
ittus <vuminhthang.cm@gmail.com>
2016-06-28 01:23:40 +0800

    add allauth

--- stripe/urls.py
+++ stripe/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^$', 'profiles.views.home', name='home'),
     url(r'^about/$', 'profiles.views.about', name='about'),
     url(r'^contact/$', 'contact.views.contact', name='contact'),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
